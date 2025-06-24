439dc20c5756ecbbaacb038ce68405c73cfbdcd5
Aleksander Płomiński <plominski.aleksander@gmail.com>
2025-06-24 11:53:22 +0200

    Added C-Compatible API
    This is a test.

--- library.cpp
+++ library.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <locale>
+#include <string.h>
 #include <vector>
 #include <unordered_map>
 
@@ -22,12 +23,12 @@ void Translation::addLanguage(std::string language_name,std::string prefix){
     TranslationLanguage language;
     language.name = language_name;
     language.prefix = prefix;
-    this->avalibleLanguages.push_back(language.prefix);
+    this->availableLanguages.push_back(language.prefix);
     this->languages[language.prefix] = language;
 }
 
 std::string Translation::getByKey(std::string key) {
-    for (std::string language : this->avalibleLanguages) {
+    for (std::string language : this->availableLanguages) {
         if (language == this->preferredLanguage) {
             TranslationLanguage selected = this->languages[this->preferredLanguage];
             return selected.translations[key];
@@ -50,7 +51,7 @@ void Translation::addPhrase(std::string phrase_key, std::string translated_phras
 
 
 void Translation::stdcoutByKey(std::string key,bool withEndl) {
-    for (std::string language : this->avalibleLanguages) {
+    for (std::string language : this->availableLanguages) {
         if (language == this->preferredLanguage) {
             TranslationLanguage selected = this->languages[this->preferredLanguage];
             if (withEndl) std::cout << selected.translations[key] << std::endl;
@@ -65,7 +66,7 @@ void Translation::stdcoutByKey(std::string key,bool withEndl) {
 }
 
 void Translation::stdcerrByKey(std::string key,bool withEndl) {
-    for (std::string language : this->avalibleLanguages) {
+    for (std::string language : this->availableLanguages) {
         if (language == this->preferredLanguage) {
             TranslationLanguage selected = this->languages[this->preferredLanguage];
             if (withEndl) std::cerr << selected.translations[key] << std::endl;
@@ -80,7 +81,7 @@ void Translation::stdcerrByKey(std::string key,bool withEndl) {
 }
 
 void Translation::stdclogByKey(std::string key,bool withEndl) {
-    for (std::string language : this->avalibleLanguages) {
+    for (std::string language : this->availableLanguages) {
         if (language == this->preferredLanguage) {
             TranslationLanguage selected = this->languages[this->preferredLanguage];
             if (withEndl) std::clog << selected.translations[key] << std::endl;
@@ -98,7 +99,72 @@ void Translation::changeLanguage(std::string perfix) {
     this->preferredLanguage = perfix;
 }
 
+Translation_handle* translation_creation(const char* defaultLanguage) {
+    Translation_handle* handle = new Translation_handle;
+    handle->ptr = new Translation(std::string(defaultLanguage), false);
+    return handle;
+}
+void translation_addLanguage(Translation_handle* handle, const char* language, const char* prefix) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->addLanguage(std::string(language), std::string(prefix));
+    }
+}
+void translation_addPhrase(Translation_handle* handle, const char* phrase_key,  const char* translated_phase, const char* language) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->addPhrase(std::string(phrase_key), std::string(translated_phase), std::string(language));
+    }
+}
+const char* translation_getByKey(Translation_handle* handle, const char* key) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->getByKey(std::string(key));
+    }
+}
 
+void stdcoutByKey_narg(Translation_handle* handle, const char* key) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->stdcoutByKey(key, true);
+    }
+}
+void stdcerrByKey_narg(Translation_handle* handle, const char* key) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->stdcerrByKey(key, true);
+    }
+}
+void stdclogByKey_narg(Translation_handle* handle, const char* key){
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->stdclogByKey(key, true);
+    }
+}
 
+void stdcoutByKey_arg(Translation_handle* handle, const char* key, bool endl) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->stdcoutByKey(key, endl);
+    }
+}
+void stdcerrByKey_arg(Translation_handle* handle, const char* key, bool endl) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->stdcerrByKey(key, endl);
+    }
+}
+void stdclogByKey_arg(Translation_handle* handle, const char* key, bool endl) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->stdclogByKey(key, endl);
+    }
+}
 
+void translation_changeLanguage(Translation_handle* handle, const char* prefix) {
+    if (handle && handle->ptr) {
+        static_cast<Translation*>(handle->ptr)->changeLanguage(std::string(prefix));
+    }
+}
 
+const char **translation_availableLanguages(Translation_handle *handle, size_t &outSize) {
+    if (handle && handle->ptr) {
+        outSize = static_cast<Translation*>(handle->ptr)->availableLanguages.size();
+        const char** arr = (const char**)malloc(sizeof(char*) * outSize);
+        for (size_t i = 0; i < outSize; ++i) {
+            arr[i] = strdup(static_cast<Translation*>(handle->ptr)->availableLanguages[i].c_str());
+        }
+        return arr;
+    }
+}
--- library.h
+++ library.h
@@ -1,11 +1,14 @@
 #ifndef OPEN_TRANSLATION_SERVER_LIBRARY_H
 #define OPEN_TRANSLATION_SERVER_LIBRARY_H
 
+#ifdef __cplusplus
 #include <iostream>
 #include <locale>
 #include <vector>
 #include <unordered_map>
+#endif
 
+#ifdef __cplusplus
 class Translation {
 public:
     Translation(std::string defaultLanguage, bool throwing = false);
@@ -17,8 +20,8 @@ public:
     void stdcerrByKey(std::string key, bool withEndl = true);
     void stdclogByKey(std::string key, bool withEndl = true);
 
-    void changeLanguage(std::string perfix);
-    std::vector<std::string> avalibleLanguages;
+    void changeLanguage(std::string prefix);
+    std::vector<std::string> availableLanguages;
 private:
     bool throwing;
     struct TranslationLanguage {
@@ -32,5 +35,31 @@ private:
     std::string defaultLanguage;
     std::unordered_map<std::string,TranslationLanguage> languages;
 };
+#endif //__cplusplus
+
+//C-API
+typedef struct {
+    void* ptr;
+}Translation_handle;
+
+void translation_creation(Translation_handle* handle, const char* defaultLanguage);
+void translation_addLanguage(Translation_handle* handle, const char* language, const char* prefix);
+void translation_addPhrase(Translation_handle* handle, const char* phrase_key,  const char* translated_phase, const char* language);
+const char* translation_getByKey(Translation_handle* handle, const char* key);
+
+void translation_stdcoutByKey_narg(Translation_handle* handle, const char* key);
+void translation_stdcerrByKey_narg(Translation_handle* handle, const char* key);
+void translation_stdclogByKey_narg(Translation_handle* handle, const char* key);
+
+void translation_stdcoutByKey_arg(Translation_handle* handle, const char* key, bool endl);
+void translation_stdcerrByKey_arg(Translation_handle* handle, const char* key, bool endl);
+void translation_stdclogByKey_arg(Translation_handle* handle, const char* key, bool endl);
+
+#define _GET_OVERRIDE(_1, _2, NAME, ...) NAME
+#define translation_stdcoutByKey(...) _GET_OVERRIDE(__VA_ARGS__, translation_stdcoutByKey_arg, translation_stdcoutByKey_narg)(__VA_ARGS__)
+#define translation_stdcerrByKey(...) _GET_OVERRIDE(__VA_ARGS__, translation_stdcerrByKey_arg, translation_stdcerrByKey_narg)(__VA_ARGS__)
+
+void translation_changeLanguage(Translation_handle* handle, const char* prefix);
+const char **translation_availableLanguages(Translation_handle *handle, size_t &outSize);
 
 #endif //OPEN_TRANSLATION_SERVER_LIBRARY_H
